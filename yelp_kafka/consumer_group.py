d455cb4e80e65d3cbe453fd3341976ffa63cd128
Enrico Canzonieri <enrico@yelp.com>
2015-04-27 13:35:59 -0700

    Fix docs

--- yelp_kafka/consumer_group.py
+++ yelp_kafka/consumer_group.py
@@ -148,7 +148,7 @@ class MultiprocessingConsumerGroup(object):
        from yelp_kafka.consumer import KafkaConsumer
        from yelp_kafka.consumer_group import MultiprocessingConsumerGroup
 
-       class MyConsumer(KafkaConsumer):
+       class MyConsumer(KafkaConsumerBase):
 
            def __init__(topic, config, partitions):
                super(MyConsumer, self).__init__(topic, config, partitions)
