1ce1c9d20fb89ee2a220d5b9b3f425911eb8ce68
Manpreet Singh <manpreet@yelp.com>
2016-08-22 14:17:47 -0700

    Bump version to add logs endpoints

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "4.15.7"
+__version__ = "4.16.0"
 version_info = tuple(map(int, __version__.split('.')))
