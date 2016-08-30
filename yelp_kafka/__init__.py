5af712cc4b76b8a7172b8a86fd982463924828c0
Enrico Canzonieri <enrico@yelp.com>
2016-08-30 11:43:19 -0700

    Bump version v4.16.1

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "4.16.0"
+__version__ = "4.16.1"
 version_info = tuple(map(int, __version__.split('.')))
