81b1f93c4ea97ddaf65cfcf411e3bb7341c4eb5c
Eric Chu <ericchu@yelp.com>
2016-09-19 14:17:47 -0700

    Bump version to 5.1.0

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "5.0.0"
+__version__ = "5.1.0"
 version_info = tuple(map(int, __version__.split('.')))
