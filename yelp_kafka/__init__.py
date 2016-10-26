0ca3ec303a1feef9ba9708e19ec79db134d3c90d
Nicholas Ngorok <nngorok@yelp.com>
2016-10-25 17:49:49 -0700

    Bump version v5.2.0

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "5.1.3"
+__version__ = "5.2.0"
 version_info = tuple(map(int, __version__.split('.')))
