a700ad093e60d626dcc020f6c416c502525a662d
Joshua Tiras <jot@yelp.com>
2016-11-04 12:54:29 -0700

    Bump version

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "5.2.0"
+__version__ = "5.2.1"
 version_info = tuple(map(int, __version__.split('.')))
