6391985ff90e7d121c95dd352cae3aa3bf96abaa
Mohammad Haseeb <mhaseeb@yelp.com>
2020-11-10 14:55:20 -0800

    Bump version to 5.2.2

--- yelp_kafka/__init__.py
+++ yelp_kafka/__init__.py
@@ -15,5 +15,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = "5.2.1"
+__version__ = "5.2.2"
 version_info = tuple(map(int, __version__.split('.')))
